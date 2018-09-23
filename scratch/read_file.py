b2bf9ca2475956ee7f9cb9106304ea4a94679be8
Fede Raimondo <federaimondo@gmail.com>
2018-09-23 20:37:19 +0200

    Data is read as in NEO

--- scratch/read_file.py
+++ scratch/read_file.py
@@ -12,14 +12,21 @@
 # permission is obtained from FastWave LLC.
 
 import numpy as np
-
+from np.testing import assert_almost_equal
 from mne.utils import logger
 
+import neo
 import trcio
 
 fname = '/Users/fraimondo/data/intra/EEG_12.TRC'
 
 raw = trcio.read_raw_trc(fname, preload=True)
-data1, times1 = raw[:, 1:10]
-data2, times2 = trcio.read_raw_trc(fname, preload=False)[:, 1:10]
+# data1, times1 = raw[:, 1:10]
+# data2, times2 = trcio.read_raw_trc(fname, preload=False)[:, 1:10]
 #
+neo_reader = neo.io.micromedio.MicromedIO(fname)
+neo_seg = neo_reader.read_segment()
+
+data = np.asarray(neo_seg.analogsignals)[0, ...].T * 1e-6
+
+assert_almost_equal(data, raw._data)
